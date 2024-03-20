3dd3aa290d0d6a727d57512e90192114af777e70
Gabrielle Coelho Silveira <gabrielle.263817@alunosatc.edu.br>
2024-03-19 21:29:44 -0300

    19-03

---
+++ 19-03.js
@@ -0,0 +1,28 @@
+//tipo number
+console.log(typeof 2);
+console.log(typeof 2.2);
+console.log(typeof -2);
+
+//operaçoes
+console.log(2 + 2);
+console.log(10 - 5);
+console.log(2 * 4);
+console.log(8 / 2);
+console.log(8 ** 2);
+console.log(8 % 2);
+
+// tipo string 
+console.log("hello world");
+console.log('hello world');
+console.log(`hello world`);
+console.log(typeof"hello");
+// concatenar strings
+console.log("qual " + "o seu " + "nome?");
+// interpolaçao
+console.log(`a soma de 5 + 5 é: ${5 + 5}`);
+
+//tipo boolean
+console.log(typeof true);
+//comparacoes
+console.log(5 <= 5);
+console.log(9 == "9");
